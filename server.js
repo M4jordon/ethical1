738138e618d49e1dd6d679bf6bdf333dd6954e6d
Kebie <kebie@Kebie>
2025-05-20 23:45:17 +0300

    ignore

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+
+const logFIle = "honeypot-log.txt"
+
+app.use((req,res,next)=>{
+	const ip =req.headers["x-forwarded-for"] ||req.connection.remoteAddress;
+	const path = req.originalUrl;
+	console.log( '{new Date().toISOString()}-{ip} access ${path}\n');
+	fs.appenFileSync(logFile,log);
+	next();
+})
+
+// fake admin panel
+app.get("/admin",(req,res,next)=>{
+	res.send("<h1>Admin panel</h1><p>Unauthorized access detected</p>")
+})
+
+// fake cpanel panel
+app.get("/cpanel",(req,res,next)=>{
+	res.send("<h1>C panel</h1><p>Unauthorized access</p>")
+})
+
+app.use("*",(req,res)=>{
+	res.send("<h1>404 Not found</h1>")
+})
+
+app.listen(8080,()=>{
+console.log("Web honeypot running at http://localhost:8080")
+})
+
+
